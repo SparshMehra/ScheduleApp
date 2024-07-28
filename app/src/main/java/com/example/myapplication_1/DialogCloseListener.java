b05d51a1efa81b20ab8f699507b620d8767b6591
Sparsh Mehra <sparshmehra0105@gmail.com>
2024-07-28 13:46:47 -0300

    Added the toDoModel and solved errors

--- app/src/main/java/com/example/myapplication_1/DialogCloseListener.java
+++ app/src/main/java/com/example/myapplication_1/DialogCloseListener.java
@@ -1,8 +1,12 @@
 package com.example.myapplication_1;
 
 import android.content.DialogInterface;
+import android.os.Bundle;
 
 public interface DialogCloseListener {
+    void onCreate(Bundle savedInstanceState);
+
+    void handleDialogClose(DialogInterface dialog);
 }   public interface DialogCloseListener{
     public void handleDialogClose(DialogInterface dialog):
 }
--- app/src/main/java/com/example/myapplication_1/MainActivity.java
+++ app/src/main/java/com/example/myapplication_1/MainActivity.java
@@ -1,26 +1,63 @@
 package com.example.myapplication_1;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myapplication_1.Adapter.ToDoAdapter;
+import com.example.myapplication_1.Util.DataBaseHandler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DialogCloseListener {
+
     private RecyclerView taskRecycleView;
+    private ToDoAdapter tasksAdapter;
+    private List<ToDoModel> taskList;
+    private DataBaseHandler db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super .onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        taskRecycleView=findViewById(R.id.taskRecycleView);
+        db= new DataBaseHandler(this);
+
+        taskList= new ArrayList<>();
+
+        taskRecycleView= taskRecycleView.findViewById(R.id.taskRecycleView);
         taskRecycleView.setLayoutManager(new LinearLayoutManager(this));
-        h;
+        tasksAdapter =new ToDoAdapter(this);
+        tasksRecycleView.setAdapter(tasksAdapter);
 
+        ToDoModel task= new ToDoModel();
+        task.setTask("This is test task");
+        task.setStatus(0);
+        task.setId(1);
 
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
 
+        tasksAdapter.setTasks(TaskList);
     }
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList= db.getAllTask();
+        Collection.reverse(TaskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
+    }
+
+
 
 }
