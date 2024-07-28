abd603fefd6fcceb9e1d4e5b786d5f29d2d2f71b
Sparsh Mehra <sparshmehra0105@gmail.com>
2024-07-27 23:09:57 -0300

    Wrong commit now write

--- app/src/main/java/com/example/myapplication_1/MainActivity.java
+++ app/src/main/java/com/example/myapplication_1/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
         taskRecycleView=findViewById(R.id.taskRecycleView);
         taskRecycleView.setLayoutManager(new LinearLayoutManager(this));
-        h;
-        
+
+
 
 
     }
